27ab0290ea5b9c92549197d76b5c33ab646ef997
gaboesquivel <contact@gaboesquivel.com>
2020-11-21 09:28:31 -0600

    wip: docker-compose and hasura configs

--- src/readers/eosio-reader/index.ts
+++ src/readers/eosio-reader/index.ts
@@ -1,63 +1,49 @@
-import { EosioReaderConfig } from 'types/config'
-import { ErrorEvent } from 'ws'
-import { createEosioShipReader } from '@blockmatic/eosio-ship-reader'
-import { EosioShipReaderConfig, ShipBlockResponse } from '@blockmatic/eosio-ship-reader'
-import { logger } from 'utils/winston'
-import { formatSecondsLeft } from 'utils/time'
+// import { EosioReaderConfig } from 'types/config'
+// import { createEosioShipReader, EosioShipReaderConfig, ShipBlockResponse } from '@blockmatic/eosio-ship-reader'
+// import { logger } from 'utils/winston'
+// import { formatSecondsLeft } from 'utils/time'
 
-const processBlock = (blockData: ShipBlockResponse) => {
-  logger.info(blockData)
-}
+// const processBlock = (blockData: ShipBlockResponse) => {
+//   logger.info(blockData)
+// }
 
-export const startEosioReader = (eosioReaderConfig: EosioReaderConfig) => {
-  const eosioShipReaderConfig: EosioShipReaderConfig = {
-    ws_url: eosioReaderConfig.ws_url,
-    ds_threads: eosioReaderConfig.ds_threads,
-    ds_experimental: eosioReaderConfig.ds_experimental,
-    tick_seconds: 1,
-    request: {
-      start_block_num: eosioReaderConfig.start_block,
-      end_block_num: eosioReaderConfig.stop_block,
-      max_messages_in_flight: eosioReaderConfig.ship_prefetch_blocks,
-      have_positions: [],
-      irreversible_only: false,
-      fetch_block: true,
-      fetch_traces: true,
-      fetch_deltas: true,
-    },
-  }
+// export const startEosioReader = (eosioReaderConfig: EosioReaderConfig) => {
+//   const eosioShipReaderConfig: EosioShipReaderConfig = {
+//     ws_url: eosioReaderConfig.ws_url,
+//     ds_threads: eosioReaderConfig.ds_threads,
+//     ds_experimental: eosioReaderConfig.ds_experimental,
+//     tick_seconds: 1,
+//     request: {
+//       start_block_num: eosioReaderConfig.start_block,
+//       end_block_num: eosioReaderConfig.stop_block,
+//       max_messages_in_flight: eosioReaderConfig.ship_prefetch_blocks,
+//       have_positions: [],
+//       irreversible_only: false,
+//       fetch_block: true,
+//       fetch_traces: true,
+//       fetch_deltas: true,
+//     },
+//   }
 
-  // TODO: read from hasura db
-  // get start block from database
-  // get have_transactions from database
+//   // TODO: read from hasura db
+//   // get start block from database
+//   // get have_transactions from database
 
-  const { connect, blocks$, close$, tick$, errors$, open$ } = createEosioShipReader(eosioShipReaderConfig)
+//   const { connect, blocks$, close$, errors$, open$ } = createEosioShipReader(eosioShipReaderConfig)
 
-  open$.subscribe(() => console.log('connection opened'))
-  errors$.subscribe((e: ErrorEvent) => console.log(e))
+//   open$.subscribe(() => console.log('connection opened'))
+//   errors$.subscribe((e: ErrorEvent) => console.log(e))
 
-  blocks$.subscribe(processBlock)
-  close$.subscribe(() => {})
+//   blocks$.subscribe(processBlock)
+//   close$.subscribe(() => {})
 
-  // TODO: this is pseudo code
-  let lastProcessedBlock: number
-  let headBlock: number
-  tick$.subscribe(({ currentBlock, lastBlock }) => {
-    const speed = (currentBlock - lastBlock) / eosioShipReaderConfig.tick_seconds
-    if (lastBlock === currentBlock && lastBlock > 0) {
-      logger.warn(`Reader ${eosioReaderConfig.name} - No blocks processed`)
-    } else if (currentBlock < lastProcessedBlock) {
-      logger.info(
-        `Reader ${eosioReaderConfig.name} - ` +
-          `Progress: ${currentBlock} / ${headBlock} ` +
-          `(${((100 * currentBlock) / headBlock).toFixed(2)}%) ` +
-          `Speed: ${speed.toFixed(1)} B/s ` +
-          `(Syncs ${formatSecondsLeft(Math.floor((headBlock - currentBlock) / speed))})`,
-      )
-    } else {
-      logger.info(`Reader ${eosioReaderConfig.name} - ` + `Current Block: ${currentBlock} ` + `Speed: ${speed.toFixed(1)} B/s `)
-    }
-  })
+//   // TODO: this is pseudo code
+//   // let lastProcessedBlock: number
+//   // let headBlock: number
 
-  connect()
+//   connect()
+// }
+
+export const startEosioReader = () => {
+  console.log('starting eosio reader...')
 }
