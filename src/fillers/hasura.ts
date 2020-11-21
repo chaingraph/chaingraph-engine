4715d0411c1d49b512e69c78bd7914a43d8b353d
gaboesquivel <contact@gaboesquivel.com>
2020-11-21 15:37:33 -0600

    wip: hasura sdk

--- src/fillers/hasura.ts
+++ src/fillers/hasura.ts
@@ -1,8 +1,8 @@
 import { GraphQLClient } from 'graphql-request'
-import { getSdk } from './generated/graphql'
+import { getSdk } from 'generated/graphql'
 
-export const BITCASH_HASURA_API = 'https://chaingraph.herokuapp.com/v1/graphql'
-export const client = new GraphQLClient(BITCASH_HASURA_API)
+export const LOCAL_HASURA_API = 'https://localhost:8088/v1/graphql'
+export const client = new GraphQLClient(LOCAL_HASURA_API)
 export const hasura = getSdk(client)
 
 export default hasura
