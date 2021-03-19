7dbba65710e0118c8d022e00fe43bdea4e3dd171
gaboesquivel <contact@gaboesquivel.com>
2021-03-19 18:49:31 +0000

    fix: token and token balance indexing

--- services/core/src/indexer/populate-tokens.ts
+++ services/core/src/indexer/populate-tokens.ts
@@ -71,6 +71,7 @@ const populateBalances = async (token_contract: string) => {
             'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
           contract: token_contract,
           account: scope,
+          symbol: balance.split(' ')[1],
           balance,
         }
       })
