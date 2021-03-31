dc77e8f4eb8916bb8f28a92ee7b7de155b07589e
gaboesquivel <contact@gaboesquivel.com>
2021-03-31 15:49:19 +0000

    chore: rollback to hasura v1.3.3

--- services/eos-api/src/scripts/start-from-snapshot.sh
+++ services/eos-api/src/scripts/start-from-snapshot.sh
@@ -6,13 +6,13 @@ source $(dirname $0)/stop.sh
 
 #echo "Donwloading eosio snapshot ..."
 # v4 snapshots are for nodeos 2.0 and 2.1, v5 snapshots are for nodeos 2.1 only
-wget https://snapshots.eosnation.io/eos-v4/latest -O latest-snapshot.bin.zst
-unzstd ./latest-snapshot.bin.zst
+wget https://snapshots.eosnation.io/eos-v4/latest -O latest-snapshot.bin.zst && \
+unzstd ./latest-snapshot.bin.zst && \
 rm ./latest-snapshot.bin.zst
 
 echo "Starting eosio service ..."
 LOGFILE=/var/log/nodeos.log
 
-nodeos --disable-replay-opts --config-dir $CONFIG_DIR --data-dir $DATA_DIR --snapshot latest-snapshot.bin \
+nodeos --disable-replay-opts --config-dir $CONFIG_DIR --data-dir $DATA_DIR --snapshot ./latest-snapshot.bin \
 >> $LOGFILE 2>&1 & echo $! > $DATA_DIR/nodeos.pid
 
