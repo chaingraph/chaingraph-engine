8d5527097e2cb78c1f1b7d4c8ad75eba5a44202a
gaboesquivel <contact@gaboesquivel.com>
2021-04-03 19:35:12 +0000

    fix: token and balances indexing

--- services/core/src/indexer/index-tables.ts
+++ services/core/src/indexer/index-tables.ts
@@ -1,4 +1,5 @@
 import { EosioReaderTableRowsStreamData } from '@blockmatic/eosio-ship-reader'
+import { asset } from 'eos-common'
 import {
   Balances_Insert_Input,
   Tokens_Insert_Input,
@@ -15,24 +16,30 @@ const upsertTableRows = async (
   tableRows$.subscribe((row) => {
     try {
       if (chaingraph_token_registry.indexOf(row.code) !== -1) {
+        console.log('--------------is token balance--------------')
+
         if (row.table === 'accounts') {
           const object: Balances_Insert_Input = {
             account: row.scope,
             balance: row.value.balance,
             chain_id: row.chain_id,
             contract: row.code,
+            symbol: row.value.balance.split(' ')[1],
           }
           hasura.upsert_balance({ object })
         }
 
         // TODO: review this
+        const tokenAsset = asset(row.value.supply)
         if (row.table === 'stat') {
           const tokenObj: Tokens_Insert_Input = {
+            chain_id: row.chain_id,
             contract: row.code,
-            // symbol: symbol.code().toString(),
-            // precision: symbol.precision(),
-            ...row,
+            symbol: tokenAsset.symbol.code().toString(),
+            precision: tokenAsset.symbol.precision(),
+            ...row.value,
           }
+          console.log({ row, tokenObj })
           hasura.upsert_token({ object: tokenObj })
         }
       } else {
