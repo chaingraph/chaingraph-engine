70607d458e64afe57979eb5d52f03f1d63d59eef
gaboesquivel <contact@gaboesquivel.com>
2021-05-04 09:59:07 -0600

    chore: update bitcashtests whitelist

--- services/core/src/whitelists/bitcashtests.ts
+++ services/core/src/whitelists/bitcashtests.ts
@@ -8,19 +8,40 @@ export const bitcashtests = [
   {
     code: 'bitcashtests',
     scope: 'bitcashtests',
-    table: 'exfees',
-    table_key: 'singleton',
+    table: 'tokens',
+    table_key: 'token_symbol-token-symbol',
   },
   {
     code: 'bitcashtests',
     scope: 'bitcashtests',
-    table: 'gpositions',
-    table_key: 'singleton',
+    table: 'deposits',
+    table_key: 'id',
   },
   {
     code: 'bitcashtests',
-    scope: 'bitcashtests',
     table: 'positions',
+    table_key: 'balance-asset-symbol',
+  },
+  {
+    code: 'bitcashtests',
+    table: 'loans',
+    table_key: 'key',
+  },
+  {
+    code: 'bitcashtests',
+    scope: 'bitcashtests',
+    table: 'p2p',
+    table_key: 'id',
+  },
+  {
+    code: 'bitcashtests',
+    table: 'ltvlevels',
+    table_key: 'token_symbol-token-symbol',
+  },
+  {
+    code: 'bitcashtests',
+    scope: 'bitcashtests',
+    table: 'actactivity',
     table_key: 'account',
   },
 ]
