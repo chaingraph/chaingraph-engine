eb259ab9a865be5e43923629b0a7f2bcf05d6c66
Gabo Esquivel <contact@gaboesquivel.com>
2021-06-23 10:52:14 -0600

    chore: consistent HASURA_GRAPHQL_ADMIN_SECRET

--- modules/hasura-client/index.ts
+++ modules/hasura-client/index.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from 'graphql-request'
 import { getSdk } from './generated/hasura-graphql'
 
 const hasuraApi = process.env.HASURA_API || 'http://localhost:8088/v1/graphql'
-const adminSecret = process.env.HASURA_ADMIN_SECRET || ''
+const adminSecret = process.env.HASURA_GRAPHQL_ADMIN_SECRET || ''
 const client = new GraphQLClient(hasuraApi)
 client.setHeader('x-hasura-admin-secret', adminSecret)
 export const hasura = getSdk(client)
