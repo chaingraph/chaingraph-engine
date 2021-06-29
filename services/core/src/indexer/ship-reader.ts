2827310b80c2bfa87cd692e19b5b83a6f00afa65
Gabo Esquivel <contact@gaboesquivel.com>
2021-06-28 18:51:52 -0600

    feat: update hasura client

--- services/core/src/indexer/ship-reader.ts
+++ services/core/src/indexer/ship-reader.ts
@@ -8,28 +8,35 @@ import { eosioApi, eosioHost, fecthAbi } from '../utils/eosio'
 import { table_rows_whitelist, actions_whitelist } from '../whitelists'
 
 export const loadReader = async () => {
+  // get the block height from the node
   const info = await fetch(`${eosioApi}/v1/chain/get_info`).then((res: any) =>
     res.json(),
   )
 
+  // contructs whitelist function callback
   const uniqueContractNames = [
-    ...new Set(table_rows_whitelist?.map((row: any) => row.code)),
+    ...new Set(table_rows_whitelist().map((row: any) => row.code)),
   ]
   const abisArr = await Promise.all(
     uniqueContractNames.map((account_name) => fecthAbi(account_name)),
   )
 
-  const contract_abis: EosioReaderAbisMap = new Map()
-  abisArr.forEach(({ account_name, abi }) =>
-    contract_abis.set(account_name, abi),
-  )
+  const contract_abis: () => EosioReaderAbisMap = () => {
+    const abis = new Map()
+    abisArr.forEach(({ account_name, abi }) => abis.set(account_name, abi))
+    return abis
+  }
 
   const eosioReaderConfig: EosioReaderConfig = {
     ws_url: `ws://${eosioHost}:8080`,
     rpc_url: eosioApi,
     ds_threads: 6,
     ds_experimental: false,
-    delta_whitelist: ['contract_table', 'contract_row', 'contract_index64'],
+    delta_whitelist: () => [
+      'contract_table',
+      'contract_row',
+      'contract_index64',
+    ],
     table_rows_whitelist,
     actions_whitelist,
     contract_abis,
