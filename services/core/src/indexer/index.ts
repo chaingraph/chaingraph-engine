a634e32f5c7e59dcba5c5052dad5d4ac7a93d41d
Gabo Esquivel <contact@gaboesquivel.com>
2021-07-21 11:54:10 -0600

    wip: full indexing

--- services/core/src/indexer/index.ts
+++ services/core/src/indexer/index.ts
@@ -1,17 +1,51 @@
 import { loadReader } from './ship-reader'
-import { indexActions } from './index-actions'
-import { indexTableRows } from './index-tables'
+// import { indexActions } from './index-actions'
+// import { indexTableRows } from './index-tables'
 import { LoaderBuffer } from './../whitelists/loader'
+import { hasura } from '../hasura'
 
 export const startIndexer = async (whitelistReader: LoaderBuffer) => {
   console.log('Starting indexer ...')
 
-  const { close$, rows$, blocks$, errors$, log$ } = await loadReader(
-    whitelistReader,
-  )
+  const { close$, blocks$, errors$, log$ } = await loadReader(whitelistReader)
 
-  indexActions(blocks$)
-  indexTableRows(rows$, whitelistReader)
+  blocks$.subscribe((block) => {
+    console.log(block)
+
+    // insert table_rows
+    const insertTableRowsObjects = block.table_rows.filter((row) => row.present)
+    hasura.query.upsert_table_rows({ objects: insertTableRowsObjects })
+
+    // delete table_rows
+    const deleteTableRows = block.table_rows
+      .filter((row) => !row.present)
+      .map((row) => {
+        return {
+          chain: {
+            _eq: 'eos',
+          },
+          contract: {
+            _eq: row.code,
+          },
+          table: {
+            _eq: row.table,
+          },
+          primary_key: {
+            _eq: row.primary_key,
+          },
+        }
+      })
+    hasura.query.delete_table_rows({ where: { _or: deleteTableRows } })
+
+    // insert block data
+
+    // insert transaction data
+
+    // insert action traces
+  })
+
+  // indexActions(blocks$)
+  // indexTableRows(rows$, whitelistReader)
 
   close$.subscribe(() => console.log('connection closed'))
 
