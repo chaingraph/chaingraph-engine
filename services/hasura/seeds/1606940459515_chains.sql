1db89a58a96c7fa8cf5174fe54e73a4f3117dbbf
gaboesquivel <contact@gaboesquivel.com>
2021-09-11 19:18:21 +0000

    chore: update chains seed

---
+++ services/hasura/seeds/1606940459515_chains.sql
@@ -0,0 +1,13 @@
+INSERT INTO chains
+VALUES (
+    'eos',
+    'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+    'http://eos.blockmatic.io'
+  );
+
+INSERT INTO chains
+VALUES (
+    'telos',
+    '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11',
+    'http://telos.blockmatic.io'
+  );
--- services/hasura/seeds/1606940459515_eos_chain.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO chains
-VALUES (
-    'eos',
-    'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-    'http://eos.chaingraph.dev'
-  );
